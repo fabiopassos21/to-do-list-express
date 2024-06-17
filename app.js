5b7716b5f3d69fcf7be6e068405b579c930aa05a
fabiopassos21 <fabiopassos1997@gmail.com>
2024-06-17 17:26:09 -0300

    'Express'

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express')
+
+const app = express(); // package
+
+app.get('/',(req,res)=>{
+   res.send('<h1>Minha lista de tarefa</h1> :/');
+}) // rota 
+
+app.listen(3000,()=>{
+    console.log("Servidor foi iniciado ") // start
+})
+
+app.get('/json',(red,res)=>{
+    res.json({title:'Tarefa x',done: true})
+})
