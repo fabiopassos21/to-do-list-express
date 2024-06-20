ad75ed5ebe7e2dd83042ec197c27e6d0555fccdd
fabiopassos21 <fabiopassos1997@gmail.com>
2024-06-20 07:15:52 -0300

    Adicionado rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,4 +7,26 @@ router.get('/',(req,res)=>{
     res.send()
 })
 
+router.post('/',(req,res) => {
+   console.log(req.body)
+   res.status(200).send(req.body);
+})
+
+router.get('/:id',(req,res)=>{
+    console.log(req.body)
+    res.send(`ID: ${req.params.id}`);
+
+})
+router.put('/:id',(req,res)=>{
+    console.log(req.body)
+    res.send(`PUT id: ${req.params.id}`);
+})
+router.delete('/:id',(req,res)=>{
+    console.log(req.body)
+    res.send(`PUT id: ${req.params.id}`);
+})
+
+
+
+
 module.exports = router
